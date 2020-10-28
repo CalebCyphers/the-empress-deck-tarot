c85a5681b57444b5a9e19123ad9fe4cbe894d657
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-10-28 16:52:36 -0600

    Add conditional - card will rotate 180 degrees if the value is reversed

--- src/DailyReading/DailyReading.js
+++ src/DailyReading/DailyReading.js
@@ -3,13 +3,25 @@ import './DailyReading.css';
 import { Col, Row, Image } from 'react-bootstrap';
 
 export default function DailyReading(props) {
-  return (
-    <div>
-      <Row>
-        <Col>
-          <Image width="350em" src={props.card.image} alt={props.card.title} rounded />
-        </Col>
-      </Row>
-    </div>
-  )
+  if (!props.card.reversed) {
+    return (
+      <div>
+        <Row>
+          <Col>
+            <Image width="350em" src={props.card.image} alt={props.card.title} rounded />
+          </Col>
+        </Row>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <Row>
+          <Col>
+            <Image width="350em" className="rotateimg180" src={props.card.image} alt={props.card.title} rounded />
+          </Col>
+        </Row>
+      </div>
+    )
+  }
 }
