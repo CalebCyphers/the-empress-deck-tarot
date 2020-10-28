d511f2301df52270d509f0a1b1923b099fb05090
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-10-28 12:40:19 -0600

    Finish cleaning code, make sure bootstrap works

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import DailyReading from '../DailyReading/DailyReading.js'
 
 function App() {
   return (
     <div className="App">
-   
+      <DailyReading />
     </div>
   );
 }
