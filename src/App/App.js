0cd5934e3e52e0b7ad6f76fe5af528fdac7fbdba
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-10-28 16:11:04 -0600

    Store the deck of cards within state

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap'
 import Deck from '../Deck.js';
 import DailyReading from '../DailyReading/DailyReading.js'
-
-const deck = new Deck()
-let randomCard = deck.getRandomCard();
 
 class App extends Component {
   state = { 
 
    }
+
+  componentDidMount(){
+    this.setState({ deck: new Deck() })
+  }
+
   render() { 
-    return ( 
-    <div className="App">
-      <img src={randomCard.image} alt={randomCard.title}></img>
-      <DailyReading />
-    </div> );
+
+    if (!this.state.deck) {
+      return (
+        <div className="App">
+          <Container>
+            <DailyReading />
+          </Container>
+        </div> );
+    } else {
+      return ( 
+        <div className="App">
+          <Container>
+            <img src={this.state.deck.getRandomCard().image} alt={this.state.deck.getRandomCard().title}></img>
+            <DailyReading />
+          </Container>
+        </div> )
+    }
   }
 }
  
