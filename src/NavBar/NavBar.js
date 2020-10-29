20d862d389b3df6d102ef1e96bbc77b4b90a7eef
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-10-29 17:22:53 -0400

    Begin formatting NavBar Component

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import { NavLink } from 'react-bootstrap';
+import './NavBar.scss';
 
 class NavBar extends Component {
     constructor() {
