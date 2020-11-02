995ed5eb3423e2355c88338451ca3d645fcc8230
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-11-02 16:12:03 -0700

    Add test to make sure the navbar is loaded properly

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -7,35 +7,42 @@ jest.mock('../fetch/fetch-requests');
 
 describe('App', () => {
 
+  let mockData = [  
+    {
+      "id": 1,
+      "reversed": true,
+      "numeral": "O",
+      "title": "The Bard",
+      "image": "https://i.imgur.com/QBKZRaT.jpg",
+      "theme": "Recklessness",
+      "description": "Now is the time to start something new, even if you are unsure of the outcome. Trust in your performance, but also take time to make sure that you are well prepared."
+    }
+  ]
+
 
   it('should render a loader when there is no deck data', () => {
     fetchCards.mockResolvedValueOnce(undefined)
     render (
       <App/>
     )
-    // test that once loader componenent has been am
     expect(screen.getByText('Loading...')).toBeInTheDocument()
-
   })
 
   it('should render a random card reading when it has deck data', async () => {
-    fetchCards.mockResolvedValueOnce([  
-      {
-        "id": 1,
-        "reversed": true,
-        "numeral": "O",
-        "title": "The Bard",
-        "image": "https://i.imgur.com/QBKZRaT.jpg",
-        "theme": "Recklessness",
-        "description": "Now is the time to start something new, even if you are unsure of the outcome. Trust in your performance, but also take time to make sure that you are well prepared."
-      }
-    ])
-
+    fetchCards.mockResolvedValueOnce(mockData)
     render(
       <App />
     )
-    const cardName = await waitFor(() => screen.getByText('The Bard (reversed)'));
+    const cardName = await waitFor(() => screen.getByText('The Bard (reversed)'))
     expect(cardName).toBeInTheDocument();
+  })
 
+  it('should render the navbar', async () => {
+    fetchCards.mockResolvedValueOnce(mockData)
+    render(
+      <App />
+    )
+    let navbar = await waitFor(() => screen.getByRole('banner'))
+    expect(navbar).toBeInTheDocument()
   })
 })
