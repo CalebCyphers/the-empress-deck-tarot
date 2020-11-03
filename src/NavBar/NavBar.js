1fc09bed4e3b32e8ae579fa0d2dd2ae262c41343
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-11-03 13:11:14 -0500

    Fix NavLink (corrected import)

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
-import { NavLink } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import './NavBar.scss';
 
 class NavBar extends Component {
@@ -14,7 +14,7 @@ class NavBar extends Component {
     render() {
         return (
             <nav className="navbar">
-                <NavLink to="/dailyreading" className="header-link header-daily-reading">Daily Reading</NavLink>
+                <NavLink to="/" className="header-link header-daily-reading">Daily Reading</NavLink>
                 <NavLink to="" className="header-link header-my-journal">My Journal</NavLink>
                 <NavLink to="/database" className="header-link header-card-database">Card Database</NavLink>
             </nav>
