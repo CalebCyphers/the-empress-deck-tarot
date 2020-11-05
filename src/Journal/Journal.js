4731f62b8987f4e9bca90e07b292f7792c1bff3f
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-11-04 21:38:39 -0500

    Make Journal Page Dynamic

--- src/Journal/Journal.js
+++ src/Journal/Journal.js
@@ -6,24 +6,27 @@ import { Link } from 'react-router-dom';
 
 export default function Journal(props) {
     return (
-      <div>
+      <div className='main'>
         {props.journalEntries.map((entry) => {
           const entryId = entry.id;
+          const entryDate = new Date(entry.id);
+          const entryDateString = entryDate.toString().substr(4, 11);
           return (
-            <p>
+            <section className='journal-entry'>
               <Row className="justify-content-md-center">
                 <Col md="auto">
-                  <Card className="mb-2" bg="dark" style={{ width: '18rem' }}>
+                  <Card className="mb-2" bg="dark" >
                     <Card.Img variant="top" src={entry.card.image} />
-                    <Card.Body>
+                    <Card.Body className='card-body'>
                       <Card.Title>
-                        {entry.text}
+                      <p className='entry-text'>{entry.text}</p>
                       </Card.Title>
+                      <p className='entry-date'>{entryDateString}</p>
                     </Card.Body>
                   </Card>
                 </Col>
               </Row>
-            </p>
+            </section>
           )
         })
         }
